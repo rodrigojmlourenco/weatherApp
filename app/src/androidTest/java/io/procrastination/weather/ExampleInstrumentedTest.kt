b5cd43732ef28a25a768d15d598365ed36d82ad8
Rodrigo Lourenco <rl01803@rmn.com>
2018-12-19 13:11:22 +0000

    [QA] Setup for AndroidX instrumented tests

--- app/src/androidTest/java/io/procrastination/weather/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/procrastination/weather/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
-package io.procrastination.sample
+package io.procrastination.weather
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,11 +13,11 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest() {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("io.procrastination.sample", appContext.packageName)
+        assertEquals("io.procrastination.weather", appContext.packageName)
     }
 }
---
+++ app/src/androidTest/java/io/procrastination/weather/view/splash/SplashActivityTest.kt
@@ -0,0 +1,12 @@
+package io.procrastination.weather.view.splash
+
+import org.junit.Assert
+import org.junit.Test
+
+class SplashActivityTest {
+
+    @Test
+    fun testSomething(){
+        Assert.assertTrue(true)
+    }
+}
