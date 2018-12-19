93deb1c86a38191b585f1194bcb4da526bffd649
Rodrigo Lourenco <rl01803@rmn.com>
2018-12-19 15:11:48 +0000

    [QA] Spoon tests running

--- app/src/androidTest/java/io/procrastination/weather/view/splash/SplashActivityTest.kt
+++ app/src/androidTest/java/io/procrastination/weather/view/splash/SplashActivityTest.kt
@@ -1,12 +1,25 @@
 package io.procrastination.weather.view.splash
 
+import android.content.Intent
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.squareup.spoon.Spoon
 import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class SplashActivityTest {
+
+    @get:Rule val activityTest : ActivityTestRule<SplashActivity> = ActivityTestRule(SplashActivity::class.java, false, false)
 
     @Test
     fun testSomething(){
+        val activity : SplashActivity = activityTest.launchActivity(Intent())
+        Spoon.screenshot(activity, "_start")
         Assert.assertTrue(true)
     }
+
+
 }
